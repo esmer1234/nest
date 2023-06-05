6452fad779ae2d2c9b7b4b12426219e9aeef9efa
Esmer Nagizade <esmernagizade@gmail.com>
2023-06-05 14:43:06 +0400

    giris.js faylinda deyisiklikler etdim

---
+++ giris.js
@@ -0,0 +1,19 @@
+document.write("salam")
+console.log(6);
+alert("xosh")
+var a = 6;
+var b = 7;
+c = a + b;
+d = a - b;
+e = a * b;
+f = a / b;
+g = a % b;
+h = a ** b;
+console.log(a);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(a++);
